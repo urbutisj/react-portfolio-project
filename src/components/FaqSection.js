08ac7634b0767905d356fc7af72073947d2f6070
Justinas <urbutisj@gmail.com>
2021-03-07 15:14:24 +0200

    4. FAQ Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Questions <span>FAQ</span></h2>
+            <div className="questions">
+                <h4>How Do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>Different Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
+                </div>
+            </div>
+            <div className="questions">
+                <h4>What Products do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,13 +3,14 @@ import React from 'react';
 //Page Componenents
 import AboutSection from "../components/AboutSection";
 import ServiceSection from '../components/ServicesSection';
-import ServicesSection from "../components/ServicesSection";
+import FaqSection from '../components/FaqSection';
 
 const AboutUs = () => {
     return(
         <>
             <AboutSection/>
             <ServiceSection />
+            <FaqSection /> 
         </>
     );
 }
