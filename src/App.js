5ee055a28ef99c9a8affbc73c9e621205693b484
Justinas <urbutisj@gmail.com>
2021-03-07 15:42:17 +0200

    5. Intro To Styled Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 
+//Global Style
+import GlobalStyle from './components/GlobalStyle';
 //Import Pages
 
 import AboutUs from "./pages/AboutUs";
@@ -6,6 +8,7 @@ import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs/>
     </div>
   );
