6a7c046a54c8848393d2f45c8721c10252d90f3d
Justinas <urbutisj@gmail.com>
2021-03-07 16:57:29 +0200

    7. FAQ Section Styling

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,39 +1,73 @@
 import React from 'react';
 
+import styled from 'styled-components';
+import {About} from '../styles';
+
 const FaqSection = () => {
     return (
-        <div className="faq">
+        <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="questions">
+            <div className="question">
                 <h4>How Do I start?</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-            <div className="questions">
+            <div className="question">
                 <h4>Daily Schedule</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-            <div className="questions">
+            <div className="question">
                 <h4>Different Payment Methods</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-            <div className="questions">
+            <div className="question">
                 <h4>What Products do you offer?</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, nihil!</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
-    )
-}
+        </Faq>
+    );
+};
+
+const Faq = styled(About)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #ccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }
+`;
 
 export default FaqSection;
